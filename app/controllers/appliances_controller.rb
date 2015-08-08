64fa0bb5ddfd9db0f09a7070acde71f023f57dd7
Jeremiah Jordan <jjordan@perlreason.com>
2015-08-08 07:53:41 -0700

    fixed bug caused by switching to ransack & will_paginate in the appliance show view, where targets for appliances where not paginatable and unsortable.

--- app/controllers/appliances_controller.rb
+++ app/controllers/appliances_controller.rb
@@ -9,7 +9,10 @@ class AppliancesController < ApplicationController
   end
 
   def show
+    q_params = params[:q] || {}
+    @q = Target.ransack( {appliance_id_eq: params[:id]}.merge( q_params ) )
     @appliance = Appliance.find( params[:id] )
+    @targets = @q.result.paginate( page: params[:page], per_page: 10 )
     @total_reachable = @appliance.targets.reachable.count
     @total_unreachable = @appliance.targets.unreachable.count
     @percentage_reachable = calculate_percentage( @total_reachable, (@total_reachable + @total_unreachable) )
